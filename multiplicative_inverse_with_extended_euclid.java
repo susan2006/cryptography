3727c21a498552beaa280be5e739385c733b7e92
Sudharsan R S <159531668+susan2006@users.noreply.github.com>
2026-07-24 14:06:39 +0530

    Update multiplicative_inverse_with_extended_euclid.java

--- multiplicative_inverse_with_extended_euclid.java
+++ multiplicative_inverse_with_extended_euclid.java
@@ -1,47 +1,37 @@
-public class GFG {
+import java.util.Scanner;
 
-    // Function for extended Euclidean Algorithm
-    static int gcdExtended(int a, int b, int[] x, int[] y)
-    {
-        // Base Case
-        if (a == 0) {
-            x[0] = 0;
-            y[0] = 1;
-            return b;
+public class mul_inv {
+    static int gcd(int n, int m, int[] x, int[] y){
+        if(n==0){
+            x[0]=0;
+            y[0]=1;
+            return m;
         }
-
-        // To store results of recursive call
-        int[] x1 = new int[1];
+        int[] x1=new int[1];
         int[] y1 = new int[1];
-        int gcd = gcdExtended(b % a, a, x1, y1);
+        int g = gcd(m%n,n,x1,y1);
 
-        // Update x and y using results of recursive call
-        x[0] = y1[0] - (b / a) * x1[0];
-        y[0] = x1[0];
-
-        return gcd;
+        x[0]=y1[0]-(m/n)*x1[0];
+        y[0]=x1[0];
+        return g;
     }
 
-    static int modInverse(int n, int m)
-    {
+    static int inverse(int n, int m){
         int[] x = new int[1];
         int[] y = new int[1];
 
-        int g = gcdExtended(n, m, x, y);
-        if (g != 1)
-            return -1;
-        else {
-
-            // m is added to handle negative x
-            int res = (x[0] % m + m) % m;
-            return res;
+        int g = gcd(n, m, x, y);
+        if(g!=1)  return -1;
+        else{
+            int r = (x[0]%m+m)%m;
+            return r;
         }
     }
-
-    public static void main(String[] args)
-    {
-        int n = 3, m = 11;
-
-        System.out.println(modInverse(n, m));
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter n and m for finding mul inv of n: ");
+        int n = s.nextInt(), m = s.nextInt();
+        System.out.println("Inverse of "+n+" is: "+inverse(n,m));
+        s.close();
     }
 }
