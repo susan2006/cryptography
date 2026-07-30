76820d254ce51766e85fe91bb3ac707bcfcce854
Sudharsan R S <159531668+susan2006@users.noreply.github.com>
2026-07-30 23:22:56 +0530

    Update railfence_cipher.java

--- railfence_cipher.java
+++ railfence_cipher.java
@@ -1,19 +1,9 @@
-// Java program to illustrate Rail Fence Cipher
-// Encryption and Decryption
 import java.util.Arrays;
+
 class RailFence {
 
-    // function to encrypt a message
-    public static String encryptRailFence(String text,
-                                          int key)
-    {
-
-        // create the matrix to cipher plain text
-        // key = rows , length(text) = columns
+    public static String encryptRailFence(String text, int key) {
         char[][] rail = new char[key][text.length()];
-
-        // filling the rail matrix to distinguish filled
-        // spaces from blank ones
         for (int i = 0; i < key; i++)
             Arrays.fill(rail[i], '\n');
 
@@ -21,25 +11,17 @@ class RailFence {
         int row = 0, col = 0;
 
         for (int i = 0; i < text.length(); i++) {
-
-            // check the direction of flow
-            // reverse the direction if we've just
-            // filled the top or bottom rail
             if (row == 0 || row == key - 1)
                 dirDown = !dirDown;
 
-            // fill the corresponding alphabet
             rail[row][col++] = text.charAt(i);
 
-            // find the next row using direction flag
             if (dirDown)
                 row++;
             else
                 row--;
         }
 
-        // now we can construct the cipher using the rail
-        // matrix
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < key; i++)
             for (int j = 0; j < text.length(); j++)
@@ -49,97 +31,73 @@ class RailFence {
         return result.toString();
     }
 
-    // This function receives cipher-text and key
-    // and returns the original text after decryption
-    public static String decryptRailFence(String cipher,
-                                          int key)
-    {
-
-        // create the matrix to cipher plain text
-        // key = rows , length(text) = columns
+    public static String decryptRailFence(String cipher, int key) {
         char[][] rail = new char[key][cipher.length()];
 
-        // filling the rail matrix to distinguish filled
-        // spaces from blank ones
         for (int i = 0; i < key; i++)
             Arrays.fill(rail[i], '\n');
 
-        // to find the direction
         boolean dirDown = true;
-
         int row = 0, col = 0;
 
-        // mark the places with '*'
         for (int i = 0; i < cipher.length(); i++) {
-            // check the direction of flow
             if (row == 0)
                 dirDown = true;
             if (row == key - 1)
                 dirDown = false;
 
-            // place the marker
             rail[row][col++] = '*';
 
-            // find the next row using direction flag
             if (dirDown)
                 row++;
             else
                 row--;
         }
 
-        // now we can construct the fill the rail matrix
         int index = 0;
         for (int i = 0; i < key; i++)
             for (int j = 0; j < cipher.length(); j++)
-                if (rail[i][j] == '*'
-                    && index < cipher.length())
+                if (rail[i][j] == '*' && index < cipher.length())
                     rail[i][j] = cipher.charAt(index++);
 
         StringBuilder result = new StringBuilder();
-
         row = 0;
         col = 0;
         for (int i = 0; i < cipher.length(); i++) {
-            // check the direction of flow
             if (row == 0)
                 dirDown = true;
             if (row == key - 1)
                 dirDown = false;
 
-            // place the marker
             if (rail[row][col] != '*')
                 result.append(rail[row][col++]);
 
-            // find the next row using direction flag
             if (dirDown)
                 row++;
             else
                 row--;
         }
+
         return result.toString();
     }
 
-    // driver program to check the above functions
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        String message1 = "attack at once";
+        int key1 = 2;
+        String encrypted1 = encryptRailFence(message1, key1);
 
-        // Encryption
-        System.out.println("Encrypted Message: ");
-        System.out.println(
-            encryptRailFence("attack at once", 2));
-        System.out.println(
-            encryptRailFence("GeeksforGeeks ", 3));
-        System.out.println(
-            encryptRailFence("defend the east wall", 3));
+        String message2 = "defend the east wall";
+        int key2 = 3;
+        String encrypted2 = encryptRailFence(message2, key2);
 
-        // Now decryption of the same cipher-text
-        System.out.println("\nDecrypted Message: ");
-        System.out.println(
-            decryptRailFence("atc toctaka ne", 2));
-        System.out.println(
-            decryptRailFence("GsGsekfrek eoe", 3));
-        System.out.println(
-            decryptRailFence("dnhaweedtees alf  tl", 3));
+        System.out.println("--- Message 1 ---");
+        System.out.println("Original:  " + message1);
+        System.out.println("Encrypted: " + encrypted1);
+        System.out.println("Decrypted: " + decryptRailFence(encrypted1, key1));
+
+        System.out.println("\n--- Message 2 ---");
+        System.out.println("Original:  " + message2);
+        System.out.println("Encrypted: " + encrypted2);
+        System.out.println("Decrypted: " + decryptRailFence(encrypted2, key2));
     }
 }
-// This code is contributed by Jay
